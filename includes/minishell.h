4f3f43e7dfc6a371aeea158665c21fa38a4720f9
fbelfort42 <fbelfort@student.42lyon.fr>
2023-02-19 17:54:08 +0100

    M4-17 - expand_path creation. to be tested with the needed features to check the ~ path. Also added the find_varvalue a generic version of the searching_path

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mpagani <mpagani@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fbelfort <fbelfort@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 11:37:55 by mpagani           #+#    #+#             */
-/*   Updated: 2023/02/18 11:47:37 by mpagani          ###   ########.fr       */
+/*   Updated: 2023/02/19 17:48:15 by fbelfort         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ typedef struct s_minish
 	int		file_out;
 	char	*path;
 	char	**path_dir;
+	char	**tokens;
 	char	*full_command;
 	char	**commands;
 	char	*dir_command;
@@ -41,37 +42,46 @@ typedef struct s_minish
 }	t_minish;
 
 /* settings */
+
 t_minish	*init_data(int argc);
-char		*setting_prompt(void);
+char		*setting_prompt(char **env);
 
 /* parsing */
+
 void		opening_files(t_minish *data, char *argv[], char flag);
 void		parsing_environment(t_minish *data, char *envp[]);
 char		*searching_path(char *envp[]);
 char		*find_dir_command(t_minish *data);
+char		*find_varvalue(char *var);
+void	    expand_path(t_minish data);
 
 /* Bonus features */
+
 int			checking_here_doc(int argc, char *argv[], t_minish *data);
 void		here_doc(int argc, char *argv[], t_minish *data);
 void		getting_and_writing_input_on_file(char *limiter, int fd);
 void		multiple_commands_handling(int argc, char *argv[], t_minish *data);
 
 /* communicating */
+
 void		communicating(t_minish *data, char *argv[], char *envp[]);
 void		creating_pipe(t_minish *data);
 void		creating_child(t_minish *data, int err);
 void		child_process(t_minish *data, char *argv[], char *envp[]);
 
 /* utils */
+
 void		matching_commands_with_right_path(t_minish *data, char *argv[], int pos);
 void		switching_input_output(t_minish *data, char flag);
 void		executing_command(t_minish *data, char *envp[]);
 
 /* error management */
+
 void		error_manager(int error, t_minish *data);
 void		check_error(int argc);
 
 /* memory stuff */
+
 void		closing_input_output(t_minish *data);
 void		waiting_childs_finishing(t_minish *data);
 void		free_path_dir(t_minish *data);
