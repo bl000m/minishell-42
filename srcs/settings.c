dcc71f881d3c270a69028e6361c35d552379ddd4
fbelfort42 <fbelfort@student.42lyon.fr>
2023-02-19 18:10:21 +0100

    M4-37 - dup_envp creation to do error management

--- srcs/settings.c
+++ srcs/settings.c
@@ -3,14 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   settings.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mpagani <mpagani@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fbelfort <fbelfort@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 17:43:22 by mpagani           #+#    #+#             */
-/*   Updated: 2023/02/18 11:36:36 by mpagani          ###   ########.fr       */
+/*   Updated: 2023/02/19 18:06:23 by fbelfort         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+char	**dup_envp(char *envp[])
+{
+	char	**dup;
+	int		i;
+
+	i = 0;
+	while (envp[i])
+		i++;
+	dup = ft_calloc(i + 1, sizeof(dup));
+	if (!dup)
+		return (NULL);
+	i = -1;
+	while (envp[++i])
+		dup[i] = ft_strdup(envp[i]); // add an error management to free if needed.
+	return (dup);
+}
 
 t_minish	*init_data(int argc)
 {
