649a06d1292883bf6e50c1fc82855c684d6fb11d
bl000m <mathiapagani@gmail.com>
2023-03-09 15:31:10 +0100

    cleaning

--- srcs/parsing/lexer_token_counting.c
+++ srcs/parsing/lexer_token_counting.c
@@ -6,7 +6,7 @@
 /*   By: mpagani <mpagani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 11:06:45 by mpagani           #+#    #+#             */
-/*   Updated: 2023/03/09 13:45:04 by mpagani          ###   ########.fr       */
+/*   Updated: 2023/03/09 15:30:46 by mpagani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,6 +80,5 @@ int	*tokens_counter(char *s, int *n_tokens)
 		identify_token(s, s[i], n_tokens, &i);
 		i++;
 	}
-	printf("n token = %d\n", *n_tokens);
 	return (n_tokens);
 }
