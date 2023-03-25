3b7909ffdc4e65edd0d17f15a991448a487703b8
bl000m <mathiapagani@gmail.com>
2023-03-25 16:10:36 +0100

    wip

--- srcs/parsing/lexer_utils.c
+++ srcs/parsing/lexer_utils.c
@@ -6,11 +6,41 @@
 /*   By: mpagani <mpagani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 15:34:55 by mpagani           #+#    #+#             */
-/*   Updated: 2023/03/25 13:05:10 by mpagani          ###   ########.fr       */
+/*   Updated: 2023/03/25 15:57:58 by mpagani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+int	check_if_dollar(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '$')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*duplicating_dollar(char **token, char *s, int start, int end)
+{
+	int		n_token;
+
+	n_token = 0;
+	// printf("end = %d\n", end);
+	while (s[start] && start < end)
+	{
+		*(*token + n_token) = s[start];
+		n_token++;
+		start++;
+	}
+	// printf("token = %s\n", *token);
+	return (*token);
+}
 
 char	*duplicating_token(char *s, int start, int end)
 {
@@ -22,47 +52,53 @@ char	*duplicating_token(char *s, int start, int end)
 	n_token = 0;
 	btw_double_quotes = 0;
 	btw_simple_quotes = 0;
+
 	token = malloc(sizeof(char) * (end - start + 1));
 	if (!token)
 		return (0);
-	while (s[start] && start < end)
+	if (check_if_dollar(s) && s[start] == '\'')
+		token = duplicating_dollar(&token, s, start, end);
+	else
 	{
-		if (s[start] == '\"' && s[start + 1] != '|'
-			&& s[start + 1] != '<' && s[start + 1] != '>')
+		while (s[start] && start < end)
 		{
-			// printf("start in \"= %d\n", start);
-			btw_double_quotes = 1;
-			// printf("btw_simple_quotes= %d\n", btw_simple_quotes);
-			if (btw_simple_quotes == 0)
+			if (s[start] == '\"' && s[start + 1] != '|'
+				&& s[start + 1] != '<' && s[start + 1] != '>')
+			{
+				// printf("start in \"= %d\n", start);
+				btw_double_quotes = 1;
+				// printf("btw_simple_quotes= %d\n", btw_simple_quotes);
+				if (btw_simple_quotes == 0)
+					start++;
+			}
+			if (s[start] == '\'' && s[start + 1] != '|' && btw_double_quotes == 0
+				&& s[start + 1] != '<' && s[start + 1] != '>')
+			{
+				// printf("start in \'= %d\n", start);
+				if (btw_simple_quotes == 1 && s[start] == '\'')
+					btw_simple_quotes = 0;
+				else
+					btw_simple_quotes = 1;
 				start++;
+				if (s[start] == '\'')
+					btw_simple_quotes = 0;
+			}
+			if (start == end
+				|| ((s[start] == '\"' || s[start] == '\'') && s[start + 1] == '\0'))
+				token[n_token] = 0;
+			else if ((!(btw_simple_quotes == 0 && s[start - 1] == '\'')))
+			{
+				token[n_token] = s[start];
+				n_token++;
+			}
+			// printf("start before last if = %d\n", start);
+			// printf("btw_simple_quotes = %d\n", btw_simple_quotes);
+			if (!(btw_simple_quotes == 0 && s[start - 1] == '\''))
+				start++;
+			// printf("start after last if = %d\n", start);
 		}
-		if (s[start] == '\'' && s[start + 1] != '|' && btw_double_quotes == 0
-			&& s[start + 1] != '<' && s[start + 1] != '>')
-		{
-			// printf("start in \'= %d\n", start);
-			if (btw_simple_quotes == 1 && s[start] == '\'')
-				btw_simple_quotes = 0;
-			else
-				btw_simple_quotes = 1;
-			start++;
-			if (s[start] == '\'')
-				btw_simple_quotes = 0;
-		}
-		if (start == end
-			|| ((s[start] == '\"' || s[start] == '\'') && s[start + 1] == '\0'))
-			token[n_token] = 0;
-		else if ((!(btw_simple_quotes == 0 && s[start - 1] == '\'')))
-		{
-			token[n_token] = s[start];
-			n_token++;
-		}
-		// printf("start before last if = %d\n", start);
-		// printf("btw_simple_quotes = %d\n", btw_simple_quotes);
-		if (!(btw_simple_quotes == 0 && s[start - 1] == '\''))
-			start++;
-		// printf("start after last if = %d\n", start);
+		token[n_token] = 0;
 	}
-	token[n_token] = 0;
 	return (token);
 }
 
